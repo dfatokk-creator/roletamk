3ac595b516615361dc9cd5c9592dd87cf0d9fc60
dfatokk-creator <dfatokk@gmail.com>
2026-09-18 01:37:45 -0300

    Create roleta.py

---
+++ roleta.py
@@ -0,0 +1,8 @@
+import random
+opçoes = ["sair", "assistir", "jogar", "call", "youtube", "cver",]
+escolha = random.choice(opçoes)
+nomes = ["mel", "kaue"]
+quem_escolhe = random.choice(nomes)
+
+print("a roleta escolheu:", escolha)
+print("quem vai escolher é:", quem_escolhe)
